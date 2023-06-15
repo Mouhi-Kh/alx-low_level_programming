c0078219a84dd07dd0e0134e2320875beea030cd
Mouhi-Kh <mouhiddine.2014@gmail.com>
2023-06-15 16:30:56 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a newline,to standart error.
+ * followed by a newline, to standart error.
  * Return: Always 1.
  */
 
