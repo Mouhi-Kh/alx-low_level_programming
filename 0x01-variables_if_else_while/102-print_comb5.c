efd096bdda9ba9470029cb1988013ec583ed177d
Mouhi-Kh <mouhiddine.2014@gmail.com>
2023-06-16 09:03:57 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,9 +9,9 @@
 int main(void)
 {
 	int p, q;
-	for (p = 0; p <= 98; p++)
-	{	
-	for (q = p + 1; q <= 99 ; q++)
+	for (p = 0 ; p <= 98 ; p++)
+	{
+	for (q = p + 1 ; q <= 99 ; q++)
 	{
 	putchar((p / 10) + '0');
 	putchar((p % 10) + '0');
